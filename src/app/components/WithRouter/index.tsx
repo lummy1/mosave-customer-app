2ab2bf45f03ea67b34489e88b0cbdcd25d4a03c6
Olumide Odebiyi <oluodebiyi@gmail.com>
2025-05-04 18:16:04 +0100

    rectify function display name error

--- src/app/components/WithRouter/index.tsx
+++ src/app/components/WithRouter/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter, useSearchParams } from "next/navigation";
 
-export function withRouter(Child: any) {
+export default function withRouter(Child: any) {
   return (props: any) => {
     const params = useSearchParams();
     const router = useRouter();
