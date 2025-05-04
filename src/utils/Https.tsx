f1f1becf60ecfa6fd5fb468e215c4a4da744e8be
Olumide Odebiyi <oluodebiyi@gmail.com>
2025-05-04 18:04:13 +0100

    updated storage  path

--- src/utils/Https.tsx
+++ src/utils/Https.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { get, removeItem } from "./Storage";
+import { get, removeItem } from "./storage";
 import AuthConstants from "@/redux/config/authConstant";
 import { useRouter } from "next/navigation";
 
